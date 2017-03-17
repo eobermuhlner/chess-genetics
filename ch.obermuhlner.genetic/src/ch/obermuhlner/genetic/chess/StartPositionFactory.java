c7fe507cddc282eb2a20831eef9f3629de724cf4
Eric Obermühlner <eobermuhlner@gmail.com>
2017-03-17 18:14:17 +0100

    create standard initial chess position

--- ch.obermuhlner.genetic/src/ch/obermuhlner/genetic/chess/StartPositionFactory.java
+++ ch.obermuhlner.genetic/src/ch/obermuhlner/genetic/chess/StartPositionFactory.java
@@ -7,30 +7,45 @@ import ch.obermuhlner.genetic.GenomeFactory;
 public class StartPositionFactory implements GenomeFactory<StartPosition> {
 
 	private static final char[] FIGURES = {
-			Board.BLACK_PAWN,
-			Board.BLACK_PAWN,
-			Board.BLACK_PAWN,
-			Board.BLACK_PAWN,
-			Board.BLACK_PAWN,
-			Board.BLACK_PAWN,
-			Board.BLACK_PAWN,
-			Board.BLACK_PAWN,
-			
-			Board.BLACK_ROOK,
 			Board.BLACK_ROOK,
 			Board.BLACK_KNIGHT,
-			Board.BLACK_KNIGHT,
 			Board.BLACK_BISHOP,
-			Board.BLACK_BISHOP,
-
-			Board.BLACK_KING,
 			Board.BLACK_QUEEN,
+			Board.BLACK_KING,
+			Board.BLACK_BISHOP,
+			Board.BLACK_KNIGHT,
+			Board.BLACK_ROOK,
+
+			Board.BLACK_PAWN,
+			Board.BLACK_PAWN,
+			Board.BLACK_PAWN,
+			Board.BLACK_PAWN,
+			Board.BLACK_PAWN,
+			Board.BLACK_PAWN,
+			Board.BLACK_PAWN,
+			Board.BLACK_PAWN,
 	};
 
 	private final Random random = new Random();
 	
 	@Override
 	public StartPosition createGenom() {
+//		return createRandomGenom();
+		return createStandardGenom();
+	}
+	
+	
+	private StartPosition createStandardGenom() {
+		StartPosition startPosition = new StartPosition();
+		for (int index = 0; index < FIGURES.length; index++) {
+			char figure = FIGURES[index];
+			startPosition.setField(index, figure);
+		}
+		return startPosition;
+	}
+
+
+	private StartPosition createRandomGenom() {
 		StartPosition startPosition = new StartPosition();
 
 		for (int index = 0; index < FIGURES.length; index++) {
