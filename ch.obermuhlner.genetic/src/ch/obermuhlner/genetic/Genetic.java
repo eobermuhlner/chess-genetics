ec21ac7cc1f8d259a39215d4622b6b99335974bc
OBE (Eric Obermühlner) <eric.obermuehlner@profidatagroup.com>
2017-03-17 13:00:18 +0100

    keep population as EvaluatedGenome

--- ch.obermuhlner.genetic/src/ch/obermuhlner/genetic/Genetic.java
+++ ch.obermuhlner.genetic/src/ch/obermuhlner/genetic/Genetic.java
@@ -12,13 +12,13 @@ public class Genetic<T> {
 
 	private final Random random = new Random();
 	
-	private final List<T> population = new ArrayList<>();
+	private final List<EvaluatedGenome<T>> population = new ArrayList<>();
 	
 	private double growBestFactor = 0.1;
-	private double growRandomFactor = 0.1;
-	private int populationCount = 100;
-	private int stepCount = 10000;
-	private int evaluationCount = 3;
+	private double growRandomFactor = 0.3;
+	private int populationCount = 1000;
+	private int stepCount = 100000;
+	private int evaluationCount = 100;
 	private int printCount = 3;
 	
 	public Genetic(GenomeFactory<T> factory, GenomeEvaluator<T> evaluator, GenomeMutator<T> mutator) {
@@ -30,7 +30,7 @@ public class Genetic<T> {
 	public void run() {
 		if (population.isEmpty()) {
 			for (int i = 0; i < populationCount; i++) {
-				population.add(factory.createGenom());
+				addGenome(factory.createGenom());
 			}
 		}
 		
@@ -38,75 +38,79 @@ public class Genetic<T> {
 			runStep();
 		}
 
-		System.out.println();
-		System.out.println("==========================================================");
-		for (int i = 0; i < populationCount; i++) {
-			System.out.println(population.get(i));
-		}
+		printPopulation(population.size());
 	}
 
 	private void runStep() {
-		double[] accumulatedEvaluation = evaluatePopulation();
+		evaluatePopulation();
 
-		sortPopulation(accumulatedEvaluation);
+		sortPopulation();
+		printPopulation(printCount);
 		
 		cullPopulation();
-		
-		System.out.println();
-		for (int i = 0; i < printCount; i++) {
-			System.out.println(population.get(i));
-		}
-
 	}
 
-	private double[] evaluatePopulation() {
-		double[] accumulatedEvaluation = new double[populationCount];
-		
-		for (int genomIndex1 = 0; genomIndex1 < accumulatedEvaluation.length; genomIndex1++) {
+	private void printPopulation(int count) {
+		System.out.println();
+		for (int i = 0; i < Math.min(count, population.size()); i++) {
+			System.out.println(population.get(i));
+		}
+	}
+	
+	private void addGenome(T genome) {
+		population.add(new EvaluatedGenome<T>(genome));
+	}
+	
+	private void evaluatePopulation() {
+		for (int genomIndex1 = 0; genomIndex1 < population.size(); genomIndex1++) {
 			for (int i = 0; i < evaluationCount; i++) {
 				int genomIndex2 = genomIndex1;
 				while (genomIndex2 == genomIndex1) {
 					genomIndex2 = random.nextInt(populationCount);
 				}
 				
-				T genom1 = population.get(genomIndex1);
-				T genom2 = population.get(genomIndex2);
+				EvaluatedGenome<T> genom1 = population.get(genomIndex1);
+				EvaluatedGenome<T> genom2 = population.get(genomIndex2);
 				
-				double evaluation = evaluator.evaluate(genom1, genom2);
-				accumulatedEvaluation[genomIndex1] += evaluation;
-				//accumulatedEvaluation[genomIndex2] -= evaluation;
+				double evaluation = evaluator.evaluate(genom1.genome, genom2.genome);
+				
+				genom1.value += evaluation;
+				genom1.count++;
+				
+				genom2.value -= evaluation;
+				genom2.count++;
 			}
 		}
-		return accumulatedEvaluation;
 	}
 
-	private void sortPopulation(double[] accumulatedEvaluation) {
-		List<EvaluatedGenome<T>> evaluatedGenomes = new ArrayList<>();
-		
-		for (int i = 0; i < accumulatedEvaluation.length; i++) {
-			evaluatedGenomes.add(new EvaluatedGenome<T>(population.get(i), accumulatedEvaluation[i]));
-		}
-		
-		evaluatedGenomes.sort(null);
-		
-		population.clear();
-		for (EvaluatedGenome<T> evaluatedGenome : evaluatedGenomes) {
-			population.add(evaluatedGenome.genome);
-		}
+	private void sortPopulation() {
+		population.sort(null);
 	}
 	
 	private static class EvaluatedGenome<T> implements Comparable<EvaluatedGenome<T>> {
 		public final T genome;
-		public final double value;
+		public double value;
+		public int count;
 		
-		public EvaluatedGenome(T genome, double value) {
+		public EvaluatedGenome(T genome) {
 			this.genome = genome;
-			this.value = value;
 		}
-
+		
 		@Override
 		public int compareTo(EvaluatedGenome<T> other) {
-			return Double.compare(value, other.value);
+			return -Double.compare(getAverageValue(), other.getAverageValue());
+		}
+		
+		public double getAverageValue() {
+			if (count == 0) {
+				return 0.0;
+			}
+			return value / count;
+		}
+		
+		@Override
+		public String toString() {
+			return genome + " " + getAverageValue();
 		}
 	}
 
@@ -120,13 +124,13 @@ public class Genetic<T> {
 		}
 		
 		for (int i = 0; i < growRandomCount; i++) {
-			T genome = population.get(random.nextInt(population.size()));
-			population.add(mutator.createMutated(genome));
+			T genome = population.get(random.nextInt(population.size())).genome;
+			population.add(new EvaluatedGenome<T>(mutator.createMutated(genome)));
 		}		
 
 		for (int i = 0; i < growBestCount; i++) {
-			T genome = population.get(i);
-			population.add(mutator.createMutated(genome));
+			T genome = population.get(i).genome;
+			population.add(new EvaluatedGenome<T>(mutator.createMutated(genome)));
 		}		
 	}
 
